f378863443e105f98c04bf20f563f91102fc13bb
sanjuli14 <juliocesargarciagarcia395@gmail.com>
2026-04-22 21:12:33 -0400

    mejorado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'stream_quiz';
+  router = inject(Router);
+  isOverlay = false;
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isOverlay = this.router.url === '' || this.router.url === '/';
+      
+      if (this.isOverlay) {
+        document.body.classList.add('overlay-mode');
+      } else {
+        document.body.classList.remove('overlay-mode');
+      }
+    });
+  }
 }
